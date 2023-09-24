a3658e85369e1123426b0ab24bbe93ada26634d7
Dhruv <dhru2602@gmail.com>
2023-09-24 23:25:48 +0530

    Final design some functionality remains

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,48 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import todologo from '../todo.png';
 
 export default function Todo() {
 
-    const handleClick = () => {
-        console.log("Click");
+    const [inputData, setInputData] = useState('');
+    const [itemList, setItemList] = useState([]);
+
+    const addItem = () => {
+        const trimmedInput = inputData.trim();
+
+        if (trimmedInput) {
+            if (itemList.includes(trimmedInput)) {
+                alert("Task Already Present in the List! Moving it to the top.");
+                const updatedList = [trimmedInput, ...itemList.filter((item) => item !== trimmedInput)];
+                setItemList(updatedList);
+            } else {
+                setItemList([trimmedInput, ...itemList]);
+            }
+        }
+        setInputData('');
+    };
+
+    const removeItem = (itemToRemove) => {
+        const updatedList = itemList.filter((item) => item !== itemToRemove);
+        setItemList(updatedList);
+    };
+
+    const removeAllItem = () => {
+        setItemList('');
     }
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            addItem();
+        }
+    };
 
     return (
         <>
             <div className="d-flex justify-content-center mt-5">
                 <div className="container text-center" style={{ width: "25rem" }}>
-                    <img src={todologo} className="img-fluid card-img-top" style={{ width: "10rem" }} alt="todo logo" />
+                    <img src={todologo} className="img-fluid card-img-top" style={{ width: "8rem" }} alt="todo logo" />
                     <div className="card-body">
-                        <h5 className="card-title">Add your list here</h5>
-                        <div className="card-text my-2 input-group">
-                            <input type="text" className="form-control" placeholder="Enter Your Task" />
-                            <span className="input-group-text" onClick={handleClick}>
+                        <h5 className="card-title mt-2">Add your list here</h5>
+                        <div className="card-text mt-3 input-group">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Enter Your Task"
+                                value={inputData}
+                                onChange={(e) => setInputData(e.target.value)}
+                                onKeyDown={handleKeyDown}
+                            />
+                            <span className="input-group-text" onClick={addItem}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
                                     <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
                                 </svg>
                             </span>
                         </div>
-                        <ul className="list-group text-start">
-                            <li className="list-group-item d-flex justify-content-between align-items-center">
-                                First Task
-                                <i className="bi bi-trash"></i>
-                            </li>
-                            <li className="list-group-item d-flex justify-content-between align-items-center">
-                                Second Task
-                                <i className="bi bi-trash"></i>
-                            </li>
-                            <li className="list-group-item d-flex justify-content-between align-items-center">
-                                Third Task
-                                <i className="bi bi-trash"></i>
-                            </li>
-                            <li className="list-group-item d-flex justify-content-between align-items-center">
-                                Fourth Task
-                                <i className="bi bi-trash"></i>
-                            </li>
-                        </ul>
+                        {
+                            itemList.length > 0 && <ul className={`list-group text-start ${itemList.length > 0 ? 'mt-3' : ''}`}>
+                                {itemList.map((task, index) => (
+                                    <li
+                                        key={index}
+                                        className="list-group-item d-flex justify-content-between align-items-center"
+                                    >
+                                        {task}
+                                        <i
+                                            className="bi bi-trash"
+                                            onClick={() => removeItem(task)}
+                                        ></i>
+                                    </li>
+                                ))}
+                            </ul>
+                        }
 
+                        {
+                            itemList.length > 0 && <div className="btn btn-primary my-3" onClick={removeAllItem}>Remove All</div>
+                        }
                     </div>
-                    <div className="btn btn-primary my-2">Check List</div>
                 </div>
             </div >
         </>
